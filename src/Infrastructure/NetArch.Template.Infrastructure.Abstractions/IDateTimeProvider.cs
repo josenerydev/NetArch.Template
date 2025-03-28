874a14a0be5f03f1f255e5d1bac2e46f442d6e52
josenerydev <josenerydev@gmail.com>
2025-03-28 19:26:23 -0300

    feat(infrastructure): adiciona IDateTimeProvider e implementação DateTimeProvider

---
+++ src/Infrastructure/NetArch.Template.Infrastructure.Abstractions/IDateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace NetArch.Template.Infrastructure.Abstractions
+{
+    public interface IDateTimeProvider
+    {
+        DateTime Now { get; }
+        DateTime UtcNow { get; }
+        DateTime Today { get; }
+    }
+}
---
+++ src/Infrastructure/NetArch.Template.Infrastructure/DateTimeProvider.cs
@@ -0,0 +1,9 @@
+﻿namespace NetArch.Template.Infrastructure
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTime Now => DateTime.Now;
+        public DateTime UtcNow => DateTime.UtcNow;
+        public DateTime Today => DateTime.Today;
+    }
+}
