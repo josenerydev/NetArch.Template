2ba956d2d59277f4ec7e461fc74c1e8ad8e4be89
josenerydev <josenerydev@gmail.com>
2025-03-29 13:09:18 -0300

    refactor(tests): remove testes obsoletos e atualiza referências de projeto

--- tests/NetArch.Template.ArchTests/LayerDependencyTests.cs
+++ tests/NetArch.Template.ArchTests/LayerDependencyTests.cs
@@ -1,8 +1,6 @@
 ﻿using ArchUnitNET.Domain;
-using ArchUnitNET.Fluent;
 using ArchUnitNET.Loader;
 using ArchUnitNET.xUnit;
-using Xunit;
 using static ArchUnitNET.Fluent.ArchRuleDefinition;
 
 namespace NetArch.Template.ArchTests
@@ -13,7 +11,9 @@ namespace NetArch.Template.ArchTests
             .LoadAssemblies(
                 typeof(Domain.AssemblyReference).Assembly,
                 typeof(Application.AssemblyReference).Assembly,
+                typeof(Application.Contracts.AssemblyReference).Assembly,
                 typeof(Infrastructure.AssemblyReference).Assembly,
+                typeof(Infrastructure.Abstractions.AssemblyReference).Assembly,
                 typeof(Persistence.EntityFrameworkCore.AssemblyReference).Assembly,
                 typeof(HttpApi.AssemblyReference).Assembly,
                 typeof(HttpApi.Public.AssemblyReference).Assembly
@@ -25,11 +25,12 @@ namespace NetArch.Template.ArchTests
         {
             var domainNamespace = "NetArch.Template.Domain";
             var applicationNamespace = "NetArch.Template.Application";
+            var applicationContractsNamespace = "NetArch.Template.Application.Contracts";
             var infrastructureNamespace = "NetArch.Template.Infrastructure";
+            var infrastructureAbstractionNamespace = "NetArch.Template.Infrastructure.Abstraction";
             var persistenceNamespace = "NetArch.Template.Persistence";
             var httpApiNamespace = "NetArch.Template.HttpApi";
             var hostsNamespace = "NetArch.Template.Hosts";
-
             var rule = Types()
                 .That()
                 .ResideInNamespace(domainNamespace, true)
@@ -39,7 +40,11 @@ namespace NetArch.Template.ArchTests
                         .That()
                         .ResideInNamespace(applicationNamespace, true)
                         .Or()
+                        .ResideInNamespace(applicationContractsNamespace, true)
+                        .Or()
                         .ResideInNamespace(infrastructureNamespace, true)
+                        .Or()
+                        .ResideInNamespace(infrastructureAbstractionNamespace, true)
                         .Or()
                         .ResideInNamespace(persistenceNamespace, true)
                         .Or()
@@ -47,35 +52,6 @@ namespace NetArch.Template.ArchTests
                         .Or()
                         .ResideInNamespace(hostsNamespace, true)
                 );
-
-            rule.Check(Architecture);
-        }
-
-        [Fact]
-        public void Domain_Should_Not_Depend_On_Application()
-        {
-            var rule = Types()
-                .That()
-                .ResideInNamespace("NetArch.Template.Domain", true)
-                .Should()
-                .NotDependOnAny(
-                    Types().That().ResideInNamespace("NetArch.Template.Application", true)
-                );
-
-            rule.Check(Architecture);
-        }
-
-        [Fact]
-        public void Domain_Should_Not_Depend_On_Infrastructure()
-        {
-            var rule = Types()
-                .That()
-                .ResideInNamespace("NetArch.Template.Domain", true)
-                .Should()
-                .NotDependOnAny(
-                    Types().That().ResideInNamespace("NetArch.Template.Infrastructure", true)
-                );
-
             rule.Check(Architecture);
         }
     }
